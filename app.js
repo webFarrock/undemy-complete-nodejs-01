ff8e7b224f92d1d39bf2bd4b93d43f682ed9a330
webFarrock <webFarrock@mail.ru>
2016-12-21 16:36:39 +0300

    section 18 fin

--- app.js
+++ app.js
@@ -13,13 +13,27 @@ console.log('command: ', command);
 console.log('Yargs: ', argv);
 
 if(command === 'add'){
-    notes.addNote(argv.title, argv.body);
+
+    let note = notes.addNote(argv.title, argv.body);
+    if(note){
+        console.log('Note created');
+        console.log('------------');
+        console.log('Title: ' + note.title);
+        console.log('Body: ' + note.body);
+    }else{
+        console.log('Note title taken');
+    }
+
 }else if(command === 'list'){
     notes.getAll();
 }else if(command === 'read'){
     notes.getNote(argv.title);
 }else if(command === 'remove'){
-    notes.removeNote(argv.title);
+    const noteRemoved = notes.removeNote(argv.title);
+    let message = noteRemoved ? 'Note was removed' : 'Note not found' ;
+
+    console.log(message);
+
 }else{
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,7 +1,35 @@
 console.log('starting note.js');
 
+const fs = require('fs');
+
+const fetchNotes = () => {
+    try{
+        let notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }catch(e){
+        return [];
+    }
+}
+
+const saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
 const addNote = (title, body) => {
     console.log('Adding note', title, body);
+
+    let notes   = fetchNotes();
+    let note    = {title, body};
+
+
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if(!duplicateNotes.length){
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+
 }
  
 const getAll = () =>{
@@ -13,7 +41,13 @@ const getNote = (title) =>{
 }
 
 const removeNote = (title) =>{
-    console.log(`call remove(${title}) function`);
+
+    let notes       = fetchNotes();
+    let uniqNotes   = notes.filter((note) => note.title !== title);
+
+    saveNotes(uniqNotes);
+
+    return notes.length !== uniqNotes.length;
 }
 
 
